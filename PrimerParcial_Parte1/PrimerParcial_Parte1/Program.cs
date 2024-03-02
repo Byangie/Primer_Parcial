d647b06d80fb339de55b8f8cc9e9de647bfee545
Fazt <contact@faztweb.com>
2024-03-02 10:49:41 -0600

    Primer Parcial

---
+++ PrimerParcial_Parte1/PrimerParcial_Parte1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Globalization;
+
+namespace PrimerParcial_Parte1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Prueba de cada metodo dentro del programa
+            Console.WriteLine("Prueba del Programa");
+            Console.WriteLine(Calculadora.Sumar(3, 4));
+            Console.WriteLine(Calculadora.Restar(4, 4));
+            Console.WriteLine(Calculadora.Multiplicar(4, 10));
+            Console.WriteLine(Calculadora.Dividir(9, 3));
+            Console.WriteLine("\n");
+            //Prueba piediendole al usuario los datos
+
+            //suma
+            Console.WriteLine("Prueba del Programa con datos del usuario");
+            Console.WriteLine("Suma");
+            Console.WriteLine("Ingrese el primer número");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el segundo número");
+            int num2 = int.Parse(Console.ReadLine());
+            int Suma = Calculadora.Sumar(num1, num2);
+            Console.WriteLine($"La suma de los dos numeros ingresados es {Suma}");
+
+            //Resta
+            Console.WriteLine("Resta");
+
+            Console.WriteLine("Ingrese el primer número");
+            int num3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo número");
+            int num4 = int.Parse(Console.ReadLine());
+            int Resta = Calculadora.Restar(num3, num4);
+            Console.WriteLine($"La resta de los dos numeros ingresados es {Resta}");
+
+            //Multiplicacion
+            Console.WriteLine("Multiplicacion");
+
+            Console.WriteLine("Ingrese el primer número");
+            int num5 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo número");
+            int num6 = int.Parse(Console.ReadLine());
+            int Multiplicacion = Calculadora.Multiplicar(num5, num6);
+            Console.WriteLine($"La multiplicación de los dos numeros ingresados es {Multiplicacion}");
+
+            //Division
+            Console.WriteLine("Division");
+
+            Console.WriteLine("Ingrese el primer número");
+            int num7 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo número");
+            int num8 = int.Parse(Console.ReadLine());
+            int Division = Calculadora.Dividir(num7, num8);
+            Console.WriteLine($"La division de los dos numeros ingresados es {Division}");
+
+            try
+            {
+                Console.WriteLine($"División: {Calculadora.Dividir(num7, num8)}");
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Ejecución del programa terminada");
+            }
+        }
+    }
+}
---
+++ PrimerParcial_Parte2/PrimerParcial_Parte2/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace PrimerParcial_Parte2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese la cantidad de números que desea ingresar: ");
+            int Numeros = int.Parse(Console.ReadLine());
+
+         
+            int num_par = 0;
+            int num_impar = 0;
+            int Suma_par = 0;
+            int Suma_impar = 0;
+
+            for (int i = 1; i <= Numeros; i++) 
+            {
+                Console.WriteLine("Ingrese cada uno de los números que desea ingresar");
+                int numero = int.Parse(Console.ReadLine());
+
+                if (numero % 2 == 0 )
+                {
+                    num_par++;
+                    Suma_par += numero;
+                }
+           
+                else
+                {
+                    num_impar++;
+                    Suma_impar += numero;
+                }
+
+            }
+            Console.WriteLine($"Hay {num_par} números pares y {num_impar} números impares");
+            Console.WriteLine($"La suma de todos los números pares es {Suma_par}");
+            Console.WriteLine($"La suma de todos los números impares es {Suma_impar}");
+        }
+    }
+}
